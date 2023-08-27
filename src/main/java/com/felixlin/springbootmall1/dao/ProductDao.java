205379dabee3d083c178c72451a1bf2fb570edf9
Felix0725 <felixlin725@gmail.com>
2023-08-27 11:42:14 +0800

    商品功能 - 實作查詢商品列表（基礎實作）

--- src/main/java/com/felixlin/springbootmall1/dao/ProductDao.java
+++ src/main/java/com/felixlin/springbootmall1/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.felixlin.springbootmall1.dao;
 import com.felixlin.springbootmall1.dto.ProductRequest;
 import com.felixlin.springbootmall1.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/felixlin/springbootmall1/service/ProductService.java
+++ src/main/java/com/felixlin/springbootmall1/service/ProductService.java
@@ -3,7 +3,11 @@ package com.felixlin.springbootmall1.service;
 import com.felixlin.springbootmall1.dto.ProductRequest;
 import com.felixlin.springbootmall1.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
