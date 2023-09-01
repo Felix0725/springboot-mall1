a4dd927786dceb2f1f6f35f1c7eb21921a3cfff8
Felix0725 <felixlin725@gmail.com>
2023-09-01 09:33:47 +0800

    帳號功能 - 檢查註冊的 email、隱藏返回的密碼

--- src/main/java/com/felixlin/springbootmall1/dao/UserDao.java
+++ src/main/java/com/felixlin/springbootmall1/dao/UserDao.java
@@ -6,5 +6,7 @@ import com.felixlin.springbootmall1.model.User;
 public interface UserDao {
 
     User getUserById(Integer userId);
+
+    User getUserByEmail(String email);
     Integer createUser(UserRegisterRequest userRegisterRequest);
 }
